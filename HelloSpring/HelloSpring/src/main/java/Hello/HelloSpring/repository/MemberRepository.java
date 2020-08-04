00c7e6d5c45b122223c2c5c10d0f9d521a3f470a
Woo Seung Min <smdkrak.woo@kt.com>
2020-08-04 18:01:27 +0900

    [UPGRADE] Before testing

--- HelloSpring/HelloSpring/src/main/java/Hello/HelloSpring/repository/MemberRepository.java
+++ HelloSpring/HelloSpring/src/main/java/Hello/HelloSpring/repository/MemberRepository.java
@@ -8,9 +8,12 @@ import java.util.Optional;
 public interface MemberRepository {
     Member save(Member member);
     //회원이 저장소에 저장
+
     Optional<Member> findById(Long id);
     Optional<Member> findByName(String name);
+    //Optional : ??
     //이름이나 Id로 찾아옴
+
     List<Member> findAll();
     //지금까지 모든 정보를 다 찾아옴
 }
--- HelloSpring/HelloSpring/src/main/java/Hello/HelloSpring/repository/MemoryMemberRepository.java
+++ HelloSpring/HelloSpring/src/main/java/Hello/HelloSpring/repository/MemoryMemberRepository.java
@@ -2,15 +2,13 @@ package Hello.HelloSpring.repository;
 
 import Hello.HelloSpring.domain.Member;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
+    //HashMpa : ??
     //sequence는 0,1,2... 키값을 생성해주는 애
 
     @Override
@@ -23,16 +21,23 @@ public class MemoryMemberRepository implements MemberRepository {
 
     @Override
     public Optional<Member> findById(Long id) {
-        return Optional.empty();
+        return Optional.ofNullable(store.get(id));
+        //요즘은 null이 반환될 가능성이 있으면 Optional 로 감싼다.
     }
 
     @Override
     public Optional<Member> findByName(String name) {
-        return Optional.empty();
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                //람다 함수를 사용하는 것이라고 함
+                //파라미터로 넘어온 name이 같은 것인지 확인하는 것
+                //같으면 반환
+                .findAny();
     }
 
     @Override
     public List<Member> findAll() {
-        return null;
+        return new ArrayList<>(store.values());
+        //store.values 는 member들
     }
 }
